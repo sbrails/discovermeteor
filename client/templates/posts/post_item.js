6ae7b7201ddfd4b480082cb0b826fe3c7e3fb9f7
Sebastian Brych <sebastian@legion>
2015-01-08 15:18:09 +0100

    voting

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,5 +8,21 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
+  }
+});
+
+Template.postItem.events({
+  'click .upvotable': function(event) {
+    event.preventDefault();
+    Meteor.call('upvote', this._id);
   }
 });
